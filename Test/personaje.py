a7141e4babba8061dad6de24cbfc9dc32dc2756b
Miguel Angel Vera Alva <112670367+itsantivirus@users.noreply.github.com>
2024-05-29 23:46:22 -0500

    Movimiento de Toshi Arreglado

--- Test/personaje.py
+++ Test/personaje.py
@@ -2,7 +2,7 @@ import pygame
 
 
 class Personaje:
-    def __init__(self, x, y, animation_move, animation_idle, path):
+    def __init__(self, x, y, animation_move, animation_idle, path_segments):
         self.flip = False
         self.animation_move = animation_move
         self.animation_idle = animation_idle
@@ -11,9 +11,13 @@ class Personaje:
         self.update_time = pygame.time.get_ticks()
         self.image = self.animation[self.frame]
         self.cambiar_forma(x, y, 50, 50)
-        self.path = path
+
+        self.path_segments = path_segments
+        self.current_segment_index = 0
+        self.path = self.path_segments[self.current_segment_index]
         self.current_point_index = 0
         self.moving = False
+        self.moving_backwards = False
 
     def cambiar_forma(self, x, y, width, height):
         self.forma = pygame.Rect(x, y, width, height)
@@ -22,13 +26,27 @@ class Personaje:
         if self.current_point_index < len(self.path) - 1:
             self.current_point_index += 1
             self.moving = True
-            print(f'Moving to next point: {self.path[self.current_point_index]}')
+            self.moving_backwards = False
+        else:
+            if self.current_segment_index < len(self.path_segments) - 1:
+                self.current_segment_index += 1
+                self.path = self.path_segments[self.current_segment_index]
+                self.current_point_index = 0
+                self.moving = True
+                self.moving_backwards = False
 
     def move_to_previous_point(self):
         if self.current_point_index > 0:
             self.current_point_index -= 1
             self.moving = True
-            print(f'Moving to previous point: {self.path[self.current_point_index]}')
+            self.moving_backwards = True
+        else:
+            if self.current_segment_index > 0 and not self.moving:
+                self.current_segment_index -= 1
+                self.path = self.path_segments[self.current_segment_index]
+                self.current_point_index = len(self.path) - 1
+                self.moving = True
+                self.moving_backwards = True
 
     def mover(self, target_x, target_y):
         moved = False
@@ -41,11 +59,25 @@ class Personaje:
                                self.forma.height)
             moved = True
 
-        print(f'Current position: ({self.forma.x}, {self.forma.y}), Target: ({target_x}, {target_y})')
-
         if self.forma.x == target_x and self.forma.y == target_y:
-            self.moving = False
-            print('Reached target')
+            if self.moving_backwards:
+                if self.current_point_index > 0:
+                    self.current_point_index -= 1
+                    self.moving = True
+                else:
+                    if self.current_segment_index > 0 and not self.moving:
+                        self.current_segment_index -= 1
+                        self.path = self.path_segments[self.current_segment_index]
+                        self.current_point_index = len(self.path) - 1
+                        self.moving = True
+                    else:
+                        self.moving = False
+            else:
+                if self.current_point_index < len(self.path) - 1:
+                    self.current_point_index += 1
+                    self.moving = True
+                else:
+                    self.moving = False
 
         return moved
 
--- Test/test.py
+++ Test/test.py
@@ -7,13 +7,15 @@ run = True
 
 wallpaper = pygame.image.load("mapa.png")
 
-pygame.display.set_caption("Prueba Toshi")
+pygame.display.set_caption("Stack Adventure")
+
 
 def escalar_imagen(image, scale):
     w = image.get_width()
     h = image.get_height()
-    new_image = pygame.transform.scale(image, (w * scale, h * scale))
+    new_image = pygame.transform.scale(image, (int(w * scale), int(h * scale)))
     return new_image
+
 
 animation_move = []
 animation_idle = []
@@ -28,30 +30,21 @@ for i in range(2):
     img = escalar_imagen(img, 0.04)
     animation_idle.append(img)
 
-# Definir el camino
-path = [
-    (130, 80),
-    (200, 80),
-    (440, 80),
-    (300, 250),
-    (350, 300),
-    (400, 350),
-    (450, 400),
-    (500, 450)
+path_segments = [
+    [(130, 70), (190, 70)],
+    [(190, 70), (440, 70)],
+    [(440, 70), (500, 70), (500, 140), (650, 140)],
+    [(650, 140), (685, 140), (690, 390)],
+    [(690, 390), (690, 350), (875, 350), (875, 440), (910, 440)],
+    [(910, 440), (1230, 440)]
 ]
 
-player = Personaje(path[0][0], path[0][1], animation_move, animation_idle, path)
+player = Personaje(path_segments[0][0][0], path_segments[0][0][1], animation_move, animation_idle, path_segments)
 
 reloj = pygame.time.Clock()
 
-move_up = False
-move_down = False
-move_left = False
-move_right = False
-
 while run:
     reloj.tick(30)
-    screen.fill((255, 255, 255))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
