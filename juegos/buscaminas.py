3b8037d860cc78cc1415e9648aaf8958de36ebfe
Alvaro Chavez <135331266+AlvaroChavezCc@users.noreply.github.com>
2024-06-02 17:46:16 -0500

    Buscaminas - mov

--- juegos/buscaminas.py
+++ juegos/buscaminas.py
@@ -11,25 +11,33 @@ def main_minas():
         return
 
     matriz = Matriz(tam_x, tam_y, minas)
+    posicion_x, posicion_y = 0, 0
     flag = True
     while flag and not matriz.comprobar():
-        matriz.mostrar()
-        print("Selecciona una acción:")
-        print("\t1. Revelar celda")
-        print("\t2. Colocar/Quitar bandera")
-        accion = int(input("-> Acción: "))
-        if accion == 1:
-            x = int(input("\tX: "))
-            y = int(input("\tY: "))
-            flag = matriz.revelar(x, y)
-        elif accion == 2:
-            x = int(input("\tX: "))
-            y = int(input("\tY: "))
-            matriz.colocar_bandera(x, y)
+        matriz.mostrar(posicion_x, posicion_y)
+        print("Moverse: w/a/s/d | Revelar celda: r | Colocar/Quitar bandera: f |")
+        accion = input("-> Acción: ").strip().lower()
+        if accion in ['w', 'a', 's', 'd']:
+            posicion_x, posicion_y = mover_jugador(accion, posicion_x, posicion_y, tam_x, tam_y)
+        elif accion == 'r':
+            flag = matriz.revelar(posicion_x, posicion_y)
+        elif accion == 'f':
+            matriz.colocar_bandera(posicion_x, posicion_y)
 
     if flag:
         print("Ganaste")
     else:
         print("Perdiste")
         matriz.revelar_todas_las_minas()
-        matriz.mostrar()
+        matriz.mostrar(posicion_x, posicion_y)
+
+def mover_jugador(direccion, posicion_x, posicion_y, tam_x, tam_y):
+    if direccion == 'w' and posicion_x > 0:
+        posicion_x -= 1
+    elif direccion == 'a' and posicion_y > 0:
+        posicion_y -= 1
+    elif direccion == 's' and posicion_x < tam_x - 1:
+        posicion_x += 1
+    elif direccion == 'd' and posicion_y < tam_y - 1:
+        posicion_y += 1
+    return posicion_x, posicion_y
