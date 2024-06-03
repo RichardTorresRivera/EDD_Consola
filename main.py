e7e9b1a398570ac4df38f5ecdf6910e4a6d6f3fb
RichardTorresRivera <rmtr2711@gmail.com>
2024-06-03 15:48:27 -0500

    LAst

--- main.py
+++ main.py
@@ -1,32 +1,44 @@
-from juegos.laberinto import main_lab
 from otros import entrada
-from juegos.hanoi import main_hanoi
-from juegos.decisiones import main_decisiones
+from juegos.completar_palabras import main_palabras
 from juegos.buscaminas import main_minas
+from juegos.cartas import main_cartas
+from juegos.hanoi import main_hanoi
+from juegos.laberinto import main_lab
+from juegos.decisiones import main_decisiones
 
 def menu():
     print("\n-----------------------------------")
     print("|  JUEGOS DE ESTRUCTURA DE DATOS  |")
     print("-----------------------------------")
-    print("| 1. Torres de Hanoi              |")
-    print("| 2. Decisiones de Toshi          |")
-    print("| 3. Buscaminas                   |")
-    print("| 4. Laberinto                    |")
+    print("| 1. Completar palabras           |")
+    print("| 2. Buscaminas                   |")
+    print("| 3. Emparejamiento de cartas     |")
+    print("| 4. Torres de Hanoi              |")
+    print("| 5. Laberinto                    |")
+    print("| 6. Decisiones de Toshi          |")
     print("| 0. Salir                        |")
     print("-----------------------------------\n")
-    
-while True:
-    menu()
-    opc = entrada.menu_opcion(4)
-    print()
-    if (opc == 1):
-        main_hanoi()
-    elif (opc == 2):
-        main_decisiones()
-    elif (opc == 3):
-        main_minas()
-    elif (opc == 4):
-        main_lab()
-    elif (opc == 0):
-        print("Saliendo ...")
-        break
+
+def main():
+    while True:
+        menu()
+        opc = entrada.menu_opcion(6)
+        print()
+        if (opc == 1):
+            main_palabras()
+        elif (opc == 2):
+            main_minas()
+        elif (opc == 3):
+            main_cartas()
+        elif (opc == 4):
+            main_hanoi()
+        elif (opc == 5):
+            main_lab()
+        elif (opc == 6):
+            main_decisiones()
+        elif (opc == 0):
+            print("Saliendo ...")
+            break
+
+if __name__ == '__main__':
+    main()
