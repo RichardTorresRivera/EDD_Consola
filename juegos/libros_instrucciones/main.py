ef483493b4755ed0e5d558b77b7f422532f0f699
AcherPlus <enmanuel.obando@unmsm.edu.pe>
2024-06-29 19:08:39 -0500

    libros instrucciones falta cards

--- juegos/libros_instrucciones/main.py
+++ juegos/libros_instrucciones/main.py
@@ -16,9 +16,14 @@ FPS = 60
 # Backgrounds
 backgroundDefault = pygame.image.load(os.path.join(config.LIBROS_DIR, "LibroVacio.png"))
 backgroundHanoi = pygame.image.load(os.path.join(config.LIBROS_DIR, "LibroHanoi.png"))
+backgroundDecision = pygame.image.load(os.path.join(config.LIBROS_DIR, "LibroDecision.png"))
+backgroundWords = pygame.image.load(os.path.join(config.LIBROS_DIR, "LibroWords.png"))
+backgroundMinesWeeper = pygame.image.load(os.path.join(config.LIBROS_DIR, "LibroMinesWeeper.png"))
+backgroundCards = pygame.image.load(os.path.join(config.LIBROS_DIR, "LibroVacio.png"))
+backgroundLabyrinth = pygame.image.load(os.path.join(config.LIBROS_DIR, "LibroLabyrinth.png"))
 
 # Context variable
-context = 'hanoi'
+context = 'labyrinth'
 
 # Fonts Instructions
 myFontTitles = pygame.font.Font(os.path.join(config.FONTS_DIR, "minecraft.ttf"), 30)
@@ -54,6 +59,97 @@ def hanoi_book():
     render_multiline_text(text_inst3, myFontParagraph, (0, 0, 0), screen, 710, 500, 22)
 
 
+def decision_book():
+    screen.blit(backgroundDecision, [0, 0])
+
+    text_concept = "Arboles: Estructura no\nlineal conformada\npor elementos\nllamados nodos y lineas\nllamadas ramas."
+    text_operation1 = "Cada nodo tiene un\nunico camino que\ncomienza desde el\nprimer nodo\n(raiz o padre)."
+    text_operation2 = "Arbol Binario: Mas\nconocido y usado, tiene\nmaximo 2 nodos hijos,\n2 opciones para\nrecorrer."
+    text_inst1 = "Estas en la etapa\nfinal del juego, ahora\neres tu quien decide\nel final de la aventura."
+    text_inst2 = "Escoge alguna de\nlas 2 opciones y\ndescubre a que te\nlleva cada escenario."
+    text_inst3 = "Puede ser un final\nbueno o malo.\nEscoge con sabiduria."
+
+    render_multiline_text(text_concept, myFontParagraph, (0, 0, 0), screen, 100, 190, 22)
+    render_multiline_text(text_operation1, myFontParagraph, (0, 0, 0), screen, 100, 340, 22)
+    render_multiline_text(text_operation2, myFontParagraph, (0, 0, 0), screen, 100, 500, 22)
+    render_multiline_text(text_inst1, myFontParagraph, (0, 0, 0), screen, 710, 190, 22)
+    render_multiline_text(text_inst2, myFontParagraph, (0, 0, 0), screen, 710, 340, 22)
+    render_multiline_text(text_inst3, myFontParagraph, (0, 0, 0), screen, 710, 520, 22)
+
+
+def words_book():
+    screen.blit(backgroundWords, [0, 0])
+
+    text_concept = "Vector: Estructura\nunidimensional. Se\ndefine su longitud al\ninicio, pero no\nen la ejecucion."
+    text_operation1 = "Cada elemento se\nalmacena en una\nposicion (desde 0\nhasta n)."
+    text_operation2 = "Para modificar un\nelemento, se busca\nla posicion deseada."
+    text_inst1 = "Hay letras que han\nsido atrapadas en\njaulas por los Shu."
+    text_inst2 = "Debes completar\nla palabra del\ncontenedor usando\nlas letras."
+    text_inst3 = "Pista: Cada palabra\nesta relacionada con\nlas estructuras\nde datos."
+
+    render_multiline_text(text_concept, myFontParagraph, (0, 0, 0), screen, 100, 190, 22)
+    render_multiline_text(text_operation1, myFontParagraph, (0, 0, 0), screen, 100, 370, 22)
+    render_multiline_text(text_operation2, myFontParagraph, (0, 0, 0), screen, 100, 530, 22)
+    render_multiline_text(text_inst1, myFontParagraph, (0, 0, 0), screen, 710, 190, 22)
+    render_multiline_text(text_inst2, myFontParagraph, (0, 0, 0), screen, 710, 340, 22)
+    render_multiline_text(text_inst3, myFontParagraph, (0, 0, 0), screen, 710, 520, 22)
+
+
+def minesweeper_book():
+    screen.blit(backgroundMinesWeeper, [0, 0])
+
+    text_concept = "Matriz: Estructura\nbidimensional. Define\nsu longitud al inicio."
+    text_operation1 = "Cuenta con 2 indices\n(desde 0,0 hasta n,m)."
+    text_operation2 = "Para modificar un\nelemento, se busca\nla posicion deseada."
+    text_inst1 = "Toshi llega a una\nmina, que contiene\nbombas en algunas\nposiciones."
+    text_inst2 = "Excava las posiciones\nvacias y coloca\nbanderas donde\nhaya bombas."
+    text_inst3 = "Los extremos indican\ncon un numero la\ncantidad de espacios.\nTen cuidado con\nexplotar una bomba."
+
+    render_multiline_text(text_concept, myFontParagraph, (0, 0, 0), screen, 100, 190, 22)
+    render_multiline_text(text_operation1, myFontParagraph, (0, 0, 0), screen, 100, 370, 22)
+    render_multiline_text(text_operation2, myFontParagraph, (0, 0, 0), screen, 100, 500, 22)
+    render_multiline_text(text_inst1, myFontParagraph, (0, 0, 0), screen, 710, 190, 22)
+    render_multiline_text(text_inst2, myFontParagraph, (0, 0, 0), screen, 710, 340, 22)
+    render_multiline_text(text_inst3, myFontParagraph, (0, 0, 0), screen, 710, 500, 22)
+
+
+def card_book():
+    """screen.blit(backgroundMinesWeeper, [0, 0])
+
+    text_concept = "Matriz: Estructura\nbidimensional. Define\nsu longitud al inicio."
+    text_operation1 = "Cuenta con 2 indices\n(desde 0,0 hasta n,m)."
+    text_operation2 = "Para modificar un\nelemento, se busca\nla posicion deseada.."
+    text_inst1 = "Toshi llega a una\nmina, que contiene\nbombas en algunas\nposiciones."
+    text_inst2 = "Excava las posiciones\nvacias y coloca\nbanderas donde\nhaya bombas."
+    text_inst3 = "Los extremos indican\ncon un numero la\ncantidad de espacios.\nTen cuidado con\nexplotar una bomba."
+
+    render_multiline_text(text_concept, myFontParagraph, (0, 0, 0), screen, 100, 190, 22)
+    render_multiline_text(text_operation1, myFontParagraph, (0, 0, 0), screen, 100, 370, 22)
+    render_multiline_text(text_operation2, myFontParagraph, (0, 0, 0), screen, 100, 500, 22)
+    render_multiline_text(text_inst1, myFontParagraph, (0, 0, 0), screen, 710, 190, 22)
+    render_multiline_text(text_inst2, myFontParagraph, (0, 0, 0), screen, 710, 340, 22)
+    render_multiline_text(text_inst3, myFontParagraph, (0, 0, 0), screen, 710, 500, 22)
+"""
+
+
+def labyrinth_book():
+    screen.blit(backgroundLabyrinth, [0, 0])
+
+    text_concept = "Esta formado por un\nconjunto de vertices\ny aristas."
+    text_operation1 = "Se dividen en grafos\ndirigidos (con flecha)\ny no dirigidos\n(sin flecha)."
+    text_operation2 = "El camino es la\nsecuencia de vertices\ndesde el primero\n(V0) hasta el ultimo\n(Vn)."
+    text_inst1 = "Toshi se enfrenta\na un laberinto\nmisterioso para\nllegar hacia la\naldea de los Shu."
+    text_inst2 = "Halla el camino\ncorrecto que Toshi\ndebe seguir."
+    text_inst3 = "Ten cuidado,\npuedes perderte\nen el proceso."
+
+    render_multiline_text(text_concept, myFontParagraph, (0, 0, 0), screen, 100, 190, 22)
+    render_multiline_text(text_operation1, myFontParagraph, (0, 0, 0), screen, 100, 350, 22)
+    render_multiline_text(text_operation2, myFontParagraph, (0, 0, 0), screen, 100, 490, 22)
+    render_multiline_text(text_inst1, myFontParagraph, (0, 0, 0), screen, 710, 190, 22)
+    render_multiline_text(text_inst2, myFontParagraph, (0, 0, 0), screen, 710, 360, 22)
+    render_multiline_text(text_inst3, myFontParagraph, (0, 0, 0), screen, 710, 500, 22)
+
+
 done = False
 
 while not done:
@@ -65,6 +161,16 @@ while not done:
 
     if context == 'hanoi':
         hanoi_book()
+    elif context == 'decision':
+        decision_book()
+    elif context == 'words':
+        words_book()
+    elif context == 'minesweeper':
+        minesweeper_book()
+    elif context == 'cards':
+        card_book()
+    elif context == 'labyrinth':
+        labyrinth_book()
     else:
         screen.blit(backgroundDefault, [0, 0])
 
